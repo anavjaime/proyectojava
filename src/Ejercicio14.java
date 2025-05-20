340da3d90a1454867a4e2ad011933d5ce58bca77
Ana <avjaimej@ufpso.edu.co>
2025-05-20 11:27:29 -0500

    ejercicio

--- src/Ejercicio14.java
+++
@@ -1,8 +0,0 @@
-public class Ejercicio14 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 50; i++) {
-            System.out.println("5 x" + i + " = " + 5 * i);
-        }
-    }
-
-}
